534b00904bd0963328ab58bb29e75405de5abf2f
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-11 13:18:27 +0100

    Create print_cmd.cpp

---
+++ print_cmd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/* 
+#include <iostream>: a header file library for working with input and output objects. It adds functionality to C++ programs. 
+using namespace std: use names for objects and variables from the standard library. 
+*/
+
+int main() {
+  cout << "Hello GarbyX \n\n";
+  cout << "This is a program written in C++";
+  return 0;
+}
+
+
+
+
